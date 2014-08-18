7828c0b70241cedfd4069dc6b94aa2d57f5a81c2
Ali Rizvi <aliabbasrizvi@gmail.com>
2014-08-17 18:25:46 -0700

    Generated student package with mean.io cli

---
+++ packages/students/public/routes/students.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('mean.students').config(['$stateProvider',
+  function($stateProvider) {
+    $stateProvider.state('students example page', {
+      url: '/students/example',
+      templateUrl: 'students/views/index.html'
+    });
+  }
+]);
